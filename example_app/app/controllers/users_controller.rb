827308b8b2628dcdaf4613a86ea13ebb0018dee5
hirocaster <hohtsuka@gmail.com>
2015-06-12 19:46:59 +0900

    Add use thread codes

--- example_app/app/controllers/users_controller.rb
+++ example_app/app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def index
-    render :json => User.all_shard.to_json
+    logger.info "Thread(get request):#{Thread.current.to_s}"
+
+    # results = User.find(rand(1..3))
+
+    results = Parallel.map(['a','b','c'], :in_threads=> 3 ) do |one_letter|
+                p "Thread:#{Thread.current.to_s}"
+                User.find(rand(1..3))
+              end
+
+    render :json => results.to_json
   end
 end
