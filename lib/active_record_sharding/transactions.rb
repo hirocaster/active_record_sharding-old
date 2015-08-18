b6118a72bfebf3c91a4351fe77fd8160a310f520
hirocaster <hohtsuka@gmail.com>
2015-08-18 23:26:19 +0900

    Add transaction method for sharding nodes

--- lib/active_record_sharding/transactions.rb
+++ lib/active_record_sharding/transactions.rb
@@ -4,18 +4,23 @@ module ActiveRecordSharding
 
     included do
       class << self
-        alias_method_chain :transaction, :shard
       end
     end
 
     module ClassMethods
-      def transaction_with_shard(options = {}, &block)
+      def transaction_next_shard(options = {}, &block)
         if sharding?
           self.sequence_id = current_sequence_id + 1
-          connection.transaction(options, &block)
+          connection.transaction options, &block
         else
-          transaction_without_shard options, &block
+          transaction options, &block
         end
+      end
+
+      def transaction_for_shard(shard_key, options = {}, &block)
+        return unless sharding?
+        self.sequence_id = shard_key
+        connection.transaction(options, &block)
       end
     end
   end
